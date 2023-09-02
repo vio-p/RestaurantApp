48f265e501bbdada719b076a2d4b0a91b8d6c071
vio-p <viopuscas0430@yahoo.com>
2023-09-02 13:39:01 +0300

    DONE

--- ViewModels/AdministratorReportsViewModel.cs
+++ ViewModels/AdministratorReportsViewModel.cs
@@ -12,10 +12,30 @@ class AdministratorReportsViewModel : ViewModelBase
 {
     public ObservableCollection<Tuple<Waiter, decimal>> DailyRevenues { get; }
     public ObservableCollection<Tuple<Waiter, decimal>> MonthlyRevenues { get; }
+
+    public ObservableCollection<Waiter> WaitersWithLowestMonthlyRevenue { get; set; }
+    public ObservableCollection<Waiter> WaitersWithHighestMonthlyRevenue { get; set; }
+
+    public List<Tuple<Waiter, decimal>> WaitersWithLowestRevenueOverLastSixMonths { get; set; }
+    public List<Tuple<Waiter, decimal>> WaitersWithHighestRevenueOverLastSixMonths { get; set; }
+
+    public List<Product> MostOrderedProducts { get; set; }
+
     public AdministratorReportsViewModel()
     {
         DailyRevenues = new(GetDailyRevenues(SelectedDate));
         MonthlyRevenues = new(GetMonthlyRevenues(SelectedYearAndMonth));
+
+        decimal lowestRevenue = MonthlyRevenues.Min(item => item.Item2);
+        WaitersWithLowestMonthlyRevenue = new(MonthlyRevenues.Where(item => item.Item2 == lowestRevenue).Select(item => item.Item1));
+
+        decimal highestRevenue = MonthlyRevenues.Max(item => item.Item2);
+        WaitersWithHighestMonthlyRevenue = new(MonthlyRevenues.Where(item => item.Item2 == highestRevenue).Select(item => item.Item1));
+
+        InitializeRevenueListsOverLastSixMonths();
+
+        using RestaurantContext context = new();
+        MostOrderedProducts = context.Products.Where(product => product.Active).OrderByDescending(product => product.OrderCount).Take(10).ToList();
     }
 
     private DateTime _selectedDate = DateTime.Now;
@@ -46,6 +66,20 @@ class AdministratorReportsViewModel : ViewModelBase
             MonthlyRevenues.Clear();
             var result = GetMonthlyRevenues(_selectedYearAndMonth);
             result.ToList().ForEach(item => MonthlyRevenues.Add(item));
+
+            decimal lowestRevenue = MonthlyRevenues.Min(item => item.Item2);
+            WaitersWithLowestMonthlyRevenue.Clear();
+            MonthlyRevenues.Where(item => item.Item2 == lowestRevenue)
+                .Select(item => item.Item1)
+                .ToList()
+                .ForEach(waiter => WaitersWithLowestMonthlyRevenue.Add(waiter));
+
+            decimal highestRevenue = MonthlyRevenues.Max(item => item.Item2);
+            WaitersWithHighestMonthlyRevenue.Clear();
+            MonthlyRevenues.Where(item => item.Item2 == highestRevenue)
+                .Select(item => item.Item1)
+                .ToList()
+                .ForEach(waiter => WaitersWithHighestMonthlyRevenue.Add(waiter));
 
             OnPropertyChanged(nameof(SelectedYearAndMonth));
         }
@@ -87,5 +121,51 @@ class AdministratorReportsViewModel : ViewModelBase
 
         result.ToList().ForEach(item => monthlyRevenues.Add(item));
         return monthlyRevenues;
+    }
+
+    private void InitializeRevenueListsOverLastSixMonths()
+    {
+        var revenueOverLastSixMonths = GetRevenueOverLastSixMonths();
+
+        decimal lowestRevenue = revenueOverLastSixMonths.Min(item => item.Item2);
+        WaitersWithLowestRevenueOverLastSixMonths = revenueOverLastSixMonths.Where(item => item.Item2 == lowestRevenue).ToList();
+
+        decimal highestRevenue = revenueOverLastSixMonths.Max(item => item.Item2);
+        WaitersWithHighestRevenueOverLastSixMonths = revenueOverLastSixMonths.Where(item => item.Item2 == highestRevenue).ToList();
+    }
+
+    private List<Tuple<Waiter, decimal>> GetRevenueOverLastSixMonths()
+    {
+        DateTime currentDate = DateTime.Now;
+        List<DateTime> firstDayOfLastSixMonths = new();
+
+        for (int i = 0; i < 6; i++)
+        {
+            firstDayOfLastSixMonths.Add(currentDate);
+            currentDate = currentDate.AddMonths(-1);
+            currentDate = new DateTime(currentDate.Year, currentDate.Month, 1);
+        }
+
+        List<List<Tuple<Waiter, decimal>>> revenueListOverLastSixMonths = new();
+        firstDayOfLastSixMonths.ForEach(date => revenueListOverLastSixMonths.Add(GetMonthlyRevenues(date)));
+
+        List<Tuple<Waiter, decimal>> revenueOverLastSixMonths = new();
+        foreach (var list in revenueListOverLastSixMonths)
+        {
+            foreach (var pair in list)
+            {
+                var existingPair = revenueOverLastSixMonths.FirstOrDefault(item => item.Item1.Id == pair.Item1.Id);
+                if (existingPair == null)
+                {
+                    revenueOverLastSixMonths.Add(pair);
+                }
+                else
+                {
+                    existingPair = new(existingPair.Item1, existingPair.Item2 + pair.Item2);
+                }
+            }
+        }
+
+        return revenueOverLastSixMonths;
     }
 }
